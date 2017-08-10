b487beea9ccdbe60507fa62deaf11a1c5114251d
Marcos Lisboa <marcosplisboa@yahoo.com.br>
2017-08-10 00:13:34 -0300

    Etapa 3 - Criando Tests

--- src/test/java/com/spartan/code/exemplo/ExemploApplicationTests.java
+++ src/test/java/com/spartan/code/exemplo/ExemploApplicationTests.java
@@ -1,16 +1,62 @@
 package com.spartan.code.exemplo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import com.spartan.code.exemplo.domain.Contact;
+import com.spartan.code.exemplo.domain.ContactType;
+import com.spartan.code.exemplo.service.ContactService;
+
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:test.properties")
 public class ExemploApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ContactService service;
+
+    private Long createContactId;
+
+    @Before
+    public void createContactIfNotExists() {
+        Contact contact = new Contact();
+        contact.setEmail("contact@mail.com");
+        contact.setName("Contact Test");
+        contact.setPhone("+55 31 91919191");
+        contact.setType(ContactType.WORK);
+        Contact created = service.add(contact);
+        createContactId = created.getId();
+
+    }
+
+    @After
+    public void deleteContactIfNotExists() {
+        service.delete(createContactId);
+    }
+
+    @Test
+    public void findAllTest() {
+        List<Contact> contacts = service.findAll();
+        assertThat(contacts).isNotEmpty();
+    }
+
+    @Test
+    public void findOneTest() {
+        Contact contact = service.findOne(createContactId);
+        assertThat(contact.getName()).contains("Contact");
+        assertThat(contact.getEmail()).startsWith("contact");
+        assertThat(contact.getPhone()).endsWith("9191");
+        assertThat(contact.getType()).isEqualTo(ContactType.WORK);
+    }
 
 }
