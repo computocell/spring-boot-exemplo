06275caaf69f0ee3123bac3f192522f508ad0d17
Marcos Lisboa <marcosplisboa@yahoo.com.br>
2017-08-09 22:53:06 -0300

    Etapa 1 - Removida a Configuração do Actuator

--- src/main/java/com/spartan/code/exemplo/ExemploApplication.java
+++ src/main/java/com/spartan/code/exemplo/ExemploApplication.java
@@ -3,10 +3,7 @@ package com.spartan.code.exemplo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
 @SpringBootApplication
-@EnableAdminServer
 public class ExemploApplication {
 
 	public static void main(String[] args) {
